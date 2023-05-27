cb16430e956de64364b58be3b4720fbbe90791c4
Oscar <oscar@digitalchallengers.com.mx>
2023-05-27 11:28:05 -0600

    Parte 2 ejercicio

--- JPA-UnitTests/demo/src/test/java/com/example/demo/MockTests.java
+++ JPA-UnitTests/demo/src/test/java/com/example/demo/MockTests.java
@@ -1,21 +1,25 @@
 package com.example.demo;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.example.demo.controllers.PetController;
 import com.example.demo.models.Pet;
 import com.example.demo.repositories.PetRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.ArgumentMatchers.any;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -35,43 +39,31 @@ import lombok.extern.slf4j.Slf4j;
 @TestPropertySource(locations = "classpath:application-test.properties")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK,
                 classes = DemoApplication.class)
-class DemoApplicationTests {
+public class MockTests {
+    @MockBean
+    PetRepository repository;
 
-	@Autowired
-	PetRepository repository;
+    @InjectMocks
+    @Autowired
+    PetController controller;
 
-	@Autowired
-	MockMvc mvc;
+    @Autowired
+    MockMvc mvc;
 
-	@Autowired
-	ObjectMapper objectMapper;
+    @Autowired
+    ObjectMapper objectMapper;
 
-	@Test
-	void repositoryAddTest() {
-		log.info("Probando agregar al repositorio");
+    @Test 
+    public void mockAdd() throws UnsupportedEncodingException, Exception{
+        log.info("Agregando mascota con mock repository");
 
-		Pet pet = new Pet();
+        Pet pet = new Pet();
 		pet.setName("memo");
 		pet.setAge(3);
 		pet.setSpecies("perro");
 		pet.setTag("123-ABC");
 
-		assertNull(pet.getId());
-		
-		this.repository.save(pet);
-
-		log.info("Mascota agregada con id -> {}", pet.getId());
-
-		assertNotNull(pet.getId());
-	}
-
-	@Test
-	public void mvcTest() throws UnsupportedEncodingException, Exception{
-		Pet pet = new Pet();
-		pet.setName("memo");
-		pet.setAge(3);
-		pet.setSpecies("perro");
-		pet.setTag("123-ABC");
+        given(repository.save(any())).willReturn(pet);
 
 		String requestBody = objectMapper.writeValueAsString(pet);
 		
@@ -83,5 +75,5 @@ class DemoApplicationTests {
 						.andReturn().getResponse().getContentAsString();
 		
 		log.info("Exito....");
-	}
+    }
 }
